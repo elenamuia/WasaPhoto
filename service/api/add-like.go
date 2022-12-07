f76a6679c2b8112c840e570546410ff8d6b2d3f2
elenamuiaACSAI <muia.1938610@studenti.uniroma1.it>
2022-12-07 18:38:25 +0100

    develope the api-handler file

---
+++ service/api/add-like.go
@@ -0,0 +1,11 @@
+package api
+
+import (
+	"net/http"
+
+	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api/reqcontext"
+	"github.com/julienschmidt/httprouter"
+)
+
+func (rt *_router) addLike(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
+}
---
+++ service/api/get-my-mainstream.go
@@ -0,0 +1,13 @@
+package api
+
+import (
+	"net/http"
+
+	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api/reqcontext"
+	"github.com/julienschmidt/httprouter"
+)
+
+// getContextReply is an example of HTTP endpoint that returns "Hello World!" as a plain text. The signature of this
+// handler accepts a reqcontext.RequestContext (see httpRouterHandler).
+func (rt *_router) getMyMain(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
+}
