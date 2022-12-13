5edece861d0d3f0136b0108ee1236caebdf808ba
elenamuia <muia.1938610@studenti.uniroma1.it>
2022-12-13 14:41:46 +0100

    begin functions implementation

--- service/database/put-follow.go
+++ service/database/put-follow.go
@@ -1,6 +1,21 @@
 package database
 
 // GetName is an example that shows you how to query data
-func (db *appdbimpl) PutFollow(UserID string) (ArrayofUsers []User, NumFollower int64, err error) {
-	return
+func (db *appdbimpl) PutFollow(u User) (User, error) {
+	res, err := db.c.Exec(`INSERT INTO followers (UserId) VALUES (?)`,
+		u.ID)
+
+	if err != nil {
+		return u, err
+	}
+
+	lastInsertID, err := res.LastInsertId()
+	if err != nil {
+
+		return u, err
+	}
+
+	u.ID = string(lastInsertID)
+	return u, nil
+
 }
