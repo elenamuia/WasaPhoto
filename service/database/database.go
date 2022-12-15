4242c5760161b3e300a8486ae2f3f7eb887765f3
elenamuia <muia.1938610@studenti.uniroma1.it>
2022-12-15 15:20:37 +0100

    ---

--- service/database/database.go
+++ service/database/database.go
@@ -48,6 +48,8 @@ type Photo struct {
 	numComm        int
 	ArrayofLike    []Like
 	ArrayofComment []Comment
+	datapost       string
+	UserID         int
 }
 
 type Like struct {
@@ -85,7 +87,7 @@ type AppDatabase interface {
 	PostPhoto(PhotoStructure string) (PhotoID int, err error)
 	PutFollow(follower User, followed User) error
 	UnbanUser(User) (err error)
-	GetMyMainstream(ArrayofPhotos []Photo) (err error)
+	GetMyMainstream() (ArrayofPhotos []Photo, err error)
 	Updateusername(User) (err error)
 	Ping() error
 }
--- service/database/get-my-mainstream.go
+++ service/database/get-my-mainstream.go
@@ -1,6 +1,29 @@
 package database
 
 // GetName is an example that shows you how to query data
-func (db *appdbimpl) GetMyMainstream(ArrayofPhotos []Photo) (err error) {
-	return
+func (db *appdbimpl) GetMyMainstream() (ArrayofPhotos []Photo, err error) {
+	var ret []Photo
+
+	// Plain simple SELECT query
+	rows, err := db.c.Query(`SELECT PhotoID, UserID, Photo, NumComment, NumLike, DataPost FROM Photo`)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = rows.Close() }()
+
+	// Here we read the resultset and we build the list to be returned
+	for rows.Next() {
+		var photo Photo
+		err = rows.Scan(&photo.ID, &photo.UserID, &photo.numComm, &photo.numLikes, &photo.datapost, &photo.PhotoStructure)
+		if err != nil {
+			return nil, err
+		}
+
+		ret = append(ret, photo)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return ret, nil
 }
