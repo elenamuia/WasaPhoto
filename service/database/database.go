850752d490dd3d4227ccda24c74cb6dff192ec04
elenamuia <muia.1938610@studenti.uniroma1.it>
2022-12-15 15:22:30 +0100

    !

--- service/database/database.go
+++ service/database/database.go
@@ -87,7 +87,7 @@ type AppDatabase interface {
 	PostPhoto(PhotoStructure string) (PhotoID int, err error)
 	PutFollow(follower User, followed User) error
 	UnbanUser(User) (err error)
-	GetMyMainstreamuser(User) (ArrayofPhotos []Photo, err error)
+	GetMyMainstream(User) (ArrayofPhotos []Photo, err error)
 	Updateusername(User) (err error)
 	Ping() error
 }
