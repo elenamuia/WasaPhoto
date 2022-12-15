b91a3480157a9a6977b32e7e031062dbf8df82d1
elenamuia <muia.1938610@studenti.uniroma1.it>
2022-12-15 16:33:26 +0100

    get photo

--- service/database/get-photo.go
+++ service/database/get-photo.go
@@ -1,6 +1,8 @@
 package database
 
 // GetName is an example that shows you how to query data
-func (db *appdbimpl) GetPhoto() (Photo Photo, err error) {
-	return
+func (db *appdbimpl) GetPhoto() (photo Photo, err error) {
+	var PhotoID int
+	err = db.c.QueryRow("SELECT * FROM Photo WHERE PhotoID=  ? ").Scan(&PhotoID)
+	return photo, err
 }
