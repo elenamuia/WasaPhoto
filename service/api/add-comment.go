92b92d51381de76076e0b72a16ea7a9a198d4726
elenamuia <muia.1938610@studenti.uniroma1.it>
2022-12-16 18:32:05 +0100

    d

--- service/api/add-comment.go
+++ service/api/add-comment.go
@@ -13,18 +13,15 @@ func (rt *_router) addComment(w http.ResponseWriter, r *http.Request, ps httprou
 
 	err := json.NewDecoder(r.Body).Decode(&comment)
 	if err != nil {
-		// The body was not a parseable JSON, reject it
+
 		w.WriteHeader(http.StatusBadRequest)
 		return
 
 	}
 
-	// Create the fountain in the database. Note that this function will return a new instance of the fountain with the
-	// same information, plus the ID.
 	err = rt.db.AddComment(comment.ToDatabaseComment())
 	if err != nil {
-		// In this case, we have an error on our side. Log the error (so we can be notified) and send a 500 to the user
-		// Note: we are using the "logger" inside the "ctx" (context) because the scope of this issue is the request.
+
 		ctx.Logger.WithError(err).Error("can't comment the photo")
 		w.WriteHeader(http.StatusInternalServerError)
 		return
@@ -33,7 +30,6 @@ func (rt *_router) addComment(w http.ResponseWriter, r *http.Request, ps httprou
 	// Here we can re-use `fountain` as FromDatabase is overwriting every variabile in the structure.
 	// bannedUser.FromDatabaseBanned(dbban)
 
-	// Send the output to the user.
 	w.Header().Set("Content-Type", "application/json")
 	_ = json.NewEncoder(w).Encode(comment)
 }
--- service/api/delete-comment.go
+++ service/api/delete-comment.go
@@ -15,21 +15,17 @@ func (rt *_router) deleteComment(w http.ResponseWriter, r *http.Request, ps http
 
 	err := json.NewDecoder(r.Body).Decode(&deletedComment)
 	if err != nil {
-		// The value was not uint64, reject the action indicating an error on the client side.
-		w.WriteHeader(http.StatusBadRequest)
+
 		return
 	}
 
 	err = rt.db.DeleteComment(deletedComment.ToDatabaseComment())
 	if errors.Is(err, database.ErrCommentDoesNotExist) {
-		// The fountain (indicated by `id`) does not exist, reject the action indicating an error on the client side.
+
 		w.WriteHeader(http.StatusNotFound)
 		return
 	} else if err != nil {
-		// In this case, we have an error on our side. Log the error (so we can be notified) and send a 500 to the user
-		// Note: we are using the "logger" inside the "ctx" (context) because the scope of this issue is the request.
-		// Note (2): we are adding the error and an additional field (`id`) to the log entry, so that we will receive
-		// the identifier of the fountain that triggered the error.
+
 		ctx.Logger.WithError(err).WithField("commentID", deletedComment).Error("can't delete the comment")
 		w.WriteHeader(http.StatusInternalServerError)
 		return
