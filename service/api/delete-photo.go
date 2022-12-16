7bb70d90770528b4e445e30ad91c585e319d3a6c
elenamuia <muia.1938610@studenti.uniroma1.it>
2022-12-16 19:24:43 +0100

    delete photo api

--- service/api/delete-photo.go
+++ service/api/delete-photo.go
@@ -1,11 +1,37 @@
 package api
 
 import (
+	"encoding/json"
+	"errors"
 	"net/http"
 
 	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api/reqcontext"
+	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/database"
 	"github.com/julienschmidt/httprouter"
 )
 
 func (rt *_router) deletePhoto(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
+	var deletedPhoto Photo
+
+	err := json.NewDecoder(r.Body).Decode(&deletedPhoto)
+	if err != nil {
+
+		return
+	}
+
+	err = rt.db.DeletePhoto(deletedPhoto.ToDatabasePhoto())
+	if errors.Is(err, database.ErrPhotoDoesNotExist) {
+
+		w.WriteHeader(http.StatusNotFound)
+		return
+	} else if err != nil {
+
+		ctx.Logger.WithError(err).WithField("PhotoID", deletedPhoto).Error("can't delete photo")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+
 }
