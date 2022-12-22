6250677fb8ed8be889d2a8ff9e1844fbea36307b
elenamuia <119944275+elenamuiaACSAI@users.noreply.github.com>
2022-12-22 14:55:22 +0100

    yes

--- service/api/delete-follow.go
+++ service/api/delete-follow.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"net/http"
+	"strconv"
 
 	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api/reqcontext"
 	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/database"
@@ -12,25 +13,35 @@ import (
 
 func (rt *_router) unfollow(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
 	var deletedFollow Follow
+	id, err := strconv.Atoi("id")
+	authToken := r.Header.Get("authToken")
 
-	err := json.NewDecoder(r.Body).Decode(&deletedFollow)
-	if err != nil {
+	bool, err := rt.db.CheckAuthToken(id, authToken)
 
-		return
-	}
+	if bool == true {
+		err = json.NewDecoder(r.Body).Decode(&deletedFollow)
+		if err != nil {
 
-	err = rt.db.DeleteFollow(deletedFollow.ToDatabaseFollow())
-	if errors.Is(err, database.ErrUserDoesNotExist) {
+			return
+		}
 
-		w.WriteHeader(http.StatusNotFound)
-		return
-	} else if err != nil {
+		err = rt.db.DeleteFollow(deletedFollow.ToDatabaseFollow())
+		if errors.Is(err, database.ErrUserDoesNotExist) {
 
-		ctx.Logger.WithError(err).WithField("FollowerID", deletedFollow).Error("can't unfollow")
+			w.WriteHeader(http.StatusNotFound)
+			return
+		} else if err != nil {
+
+			ctx.Logger.WithError(err).WithField("FollowerID", deletedFollow).Error("can't unfollow")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusNoContent)
+
+	} else {
+		ctx.Logger.WithError(err).Error("Uncorrect token")
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-
-	w.WriteHeader(http.StatusNoContent)
-
 }
