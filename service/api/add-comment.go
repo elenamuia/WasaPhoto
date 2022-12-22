f20c3a296a2a64e1ce4847fe6953bae44e6248be
elenamuia <119944275+elenamuiaACSAI@users.noreply.github.com>
2022-12-22 14:45:18 +0100

    authtoken add comment

--- service/api/add-comment.go
+++ service/api/add-comment.go
@@ -3,6 +3,7 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api/reqcontext"
 	"github.com/julienschmidt/httprouter"
@@ -10,30 +11,40 @@ import (
 
 func (rt *_router) addComment(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
 	var comment Comment
+	id, err := strconv.Atoi("id")
+	authToken := r.Header.Get("authToken")
 
-	err := json.NewDecoder(r.Body).Decode(&comment)
-	if err != nil {
+	bool, err := rt.db.CheckAuthToken(id, authToken)
 
-		w.WriteHeader(http.StatusBadRequest)
-		return
+	if bool == true {
+		err = json.NewDecoder(r.Body).Decode(&comment)
+		if err != nil {
 
-	}
+			w.WriteHeader(http.StatusBadRequest)
+			return
 
-	err = rt.db.AddComment(comment.ToDatabaseComment())
-	if err != nil {
+		}
 
-		ctx.Logger.WithError(err).Error("can't comment the photo")
+		err = rt.db.AddComment(comment.ToDatabaseComment())
+		if err != nil {
+
+			ctx.Logger.WithError(err).Error("can't comment the photo")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		} else if comment.UserIDPut == comment.UserIDRec {
+			ctx.Logger.WithError(err).Error("A user can't comment himself/herself")
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+
+		// Here we can re-use `fountain` as FromDatabase is overwriting every variabile in the structure.
+		// bannedUser.FromDatabaseBanned(dbban)
+
+		w.Header().Set("Content-Type", "application/json")
+		_ = json.NewEncoder(w).Encode(comment)
+	} else {
+		ctx.Logger.WithError(err).Error("Uncorrect token")
 		w.WriteHeader(http.StatusInternalServerError)
 		return
-	} else if comment.UserIDPut == comment.UserIDRec {
-		ctx.Logger.WithError(err).Error("A user can't comment himself/herself")
-		w.WriteHeader(http.StatusForbidden)
-		return
 	}
-
-	// Here we can re-use `fountain` as FromDatabase is overwriting every variabile in the structure.
-	// bannedUser.FromDatabaseBanned(dbban)
-
-	w.Header().Set("Content-Type", "application/json")
-	_ = json.NewEncoder(w).Encode(comment)
 }
--- service/database/post-photo.go
+++ service/database/post-photo.go
@@ -4,6 +4,7 @@ import "time"
 
 // GetName is an example that shows you how to query data
 func (db *appdbimpl) PostPhoto(photo Photo) (err error) {
+
 	res, err := db.c.Exec(`INSERT INTO Photo (PhotoID, UserID, Photo, NumComment, NumLike, DataPost) VALUES (?, ?, ?, ?,?,?)`,
 		photo.ID, photo.UserID, photo.PhotoStructure, photo.NumComm, photo.NumLikes, time.Now())
 	if err != nil {
