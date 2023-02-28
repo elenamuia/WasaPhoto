241390544068c4ce21f9229c3afb81f3de79ba09
elenamuia <119944275+elenamuiaACSAI@users.noreply.github.com>
2023-02-28 23:44:39 +0100

    post photo

--- service/database/post-photo.go
+++ service/database/post-photo.go
@@ -5,7 +5,7 @@ import "time"
 func (db *appdbimpl) PostPhoto(photo Photo) error {
 
 	res, err1 := db.c.Exec(`INSERT INTO Photo (PhotoID, UserID, Photo, NumComment, NumLike, DataPost) VALUES (?, ?, ?, ?,?,?)`,
-		photo.ID, photo.UserID, photo.PhotoStructure, photo.NumComm, photo.NumLikes, time.Now())
+		photo.ID, photo.UserID, photo.PhotoStructure, 0, 0, time.Now())
 	if err1 != nil {
 		return err1
 	}
