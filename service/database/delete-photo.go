43482b7dc0ff8a957f60393b8420267cca3c9add
elenamuia <119944275+elenamuiaACSAI@users.noreply.github.com>
2023-03-01 19:16:00 +0100

    fmt toltooo

--- service/database/delete-photo.go
+++ service/database/delete-photo.go
@@ -1,11 +1,8 @@
 package database
 
-import "fmt"
-
 func (db *appdbimpl) DeletePhoto(phId int) error {
 	res, err1 := db.c.Exec(`DELETE FROM Photo WHERE PhotoID=? `, phId)
 
-	fmt.Println("ciao:", phId)
 	if err1 != nil {
 		return err1
 	}
