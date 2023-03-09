1adac3729884ef000a24257a1d6b57204e15b6e3
elenamuia <119944275+elenamuiaACSAI@users.noreply.github.com>
2023-03-09 23:37:29 +0100

    ..

--- service/api/update-username.go
+++ service/api/update-username.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api/reqcontext"
 	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/database"
@@ -16,23 +17,25 @@ func (rt *_router) setMyUserName(w http.ResponseWriter, r *http.Request, ps http
 	id := ps.ByName("userid")
 
 	authToken := r.Header.Get("Authorization")
-
+	authToken = strings.Replace(authToken, "Bearer ", "", 1)
+	fmt.Println(authToken)
 	bool, err := rt.db.CheckAuthToken(authToken)
+	if err != nil {
+
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
 	if bool {
-		if err != nil {
-
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
 		var newName string
 		err2 := json.NewDecoder(r.Body).Decode(&newName)
+		fmt.Println(newName)
 		if err2 != nil {
 
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
-
+		fmt.Println(id)
 		username, err2 := rt.db.Updateusername(id, newName)
 		fmt.Println(username)
 		if errors.Is(err, database.ErrUserDoesNotExist) {
