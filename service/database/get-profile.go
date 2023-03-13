763cab0b8ae970ba342446bd4f81159fb39ed5d5
elenamuia <119944275+elenamuiaACSAI@users.noreply.github.com>
2023-03-13 20:05:06 +0100

    get profile fix

--- service/database/get-profile.go
+++ service/database/get-profile.go
@@ -1,14 +1,12 @@
 package database
 
-import "fmt"
-
 func (db *appdbimpl) GetProfile(user string) (p Profile, err error) {
 
 	p.User = user
 
 	rows1, err1 := db.c.Query(`SELECT p.PhotoID FROM Photo as p WHERE p.User = ?`, user)
 	if err1 != nil {
-		fmt.Println("err1", err1)
+
 		return p, err1
 	}
 
@@ -18,7 +16,7 @@ func (db *appdbimpl) GetProfile(user string) (p Profile, err error) {
 
 		err2 := rows1.Scan(&photoID)
 		if err2 != nil {
-			fmt.Println("err2", err2)
+
 			return p, err
 		}
 
@@ -31,30 +29,26 @@ func (db *appdbimpl) GetProfile(user string) (p Profile, err error) {
 	}
 	rows1.Close()
 
-	fmt.Println("aaaa")
-
 	rows2, err8 := db.c.Query(`SELECT Follower FROM Follower WHERE Followed = ?`, user)
 	if err8 != nil {
 		return p, err8
 	}
-	fmt.Println("bbb")
 
 	for rows2.Next() {
 		var followerid string
 		err6 := rows2.Scan(&followerid)
 		if err6 != nil {
-			fmt.Println("err6", err6)
+
 			return p, err6
 		}
 		p.Follower = append(p.Follower, followerid)
 	}
-	fmt.Println(p.Follower)
+
 	if err4 := rows2.Err(); err4 != nil {
-		fmt.Println("err4", err4)
+
 		return p, err4
 	}
-	// rows2.Close()
-	fmt.Println("ccc")
+	rows2.Close()
 
 	rows3, err3 := db.c.Query(`SELECT Followed FROM Follower WHERE Follower = ?`, user)
 	if err3 != nil {
@@ -70,12 +64,11 @@ func (db *appdbimpl) GetProfile(user string) (p Profile, err error) {
 		p.Followed = append(p.Followed, followedid)
 
 	}
-	fmt.Println("Followed: ", p.Followed)
 
 	if err5 := rows3.Err(); err5 != nil {
 		return p, err
 	}
-	// rows3.Close()
+	rows3.Close()
 
 	return p, nil
 }
