df955476829082aeaf7a9f35af2803c5a7356d08
elenamuia <119944275+elenamuiaACSAI@users.noreply.github.com>
2023-03-13 10:47:38 +0100

    fmt removal

--- service/database/get-photo.go
+++ service/database/get-photo.go
@@ -1,11 +1,9 @@
 package database
 
-import "fmt"
-
 func (db *appdbimpl) GetPhoto(photoid int) (photo Photo, err error) {
-	fmt.Println(photoid)
+
 	err1 := db.c.QueryRow("SELECT * FROM Photo WHERE PhotoID=  ? ", photoid).Scan(&photo.ID, &photo.User, &photo.PhotoStructure, &photo.Datapost)
-	fmt.Println(photo.ID)
+
 	if err1 != nil {
 		return photo, err
 	}
