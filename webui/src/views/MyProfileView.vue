31f0ba75f798f5f91a8846474a120977ee8893ba
Elena Muià <muia.1938610@studenti.uniroma1.it>
2023-05-11 12:47:24 +0200

    profile update

--- webui/src/views/MyProfileView.vue
+++ webui/src/views/MyProfileView.vue
@@ -6,21 +6,17 @@ import SideBarMenu from '../components/SideBar_Menu.vue'
 export default {
     components: {
         SideBarMenu
-  },
-  data(){
-    followerList = null,
-    followedList = null,
-    n_post = null
+    },
+    data: function() {
+      return{
+        n_follower:0,
+        n_followed:0,
+        n_posts:0,
+      }
   },
 
+}
 
-mounted() {
-    axios.get('/api/valore-dal-backend')
-      .then(response => {
-        this.valoreDallaChiamataAPI = response.data;
-      });
-  }
-};
 </script>
 
 <template >
@@ -32,33 +28,36 @@ mounted() {
         
       </SideBarMenu>
       <div class = "container-fluid" style="margin-left: 400px; margin-top: 30px; height: max-content;">
-       
         <div class="row">
+          
           <div class="col col-lg-3" style="font-size: large;">
             {{this.$current_user.id}}
+          
           </div>
-          <div class="navbar col-sm-2 ">
-            <ul class="nav" style="font-size: large;">
-              <li class="nav-item active">
-                
-
-                <a class="nav-link" href="#">Follower </a>
-              </li>
-              <div v-if="followedList">{{followedList}}</div>
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Followed </a>
-              </li>
-              <div v-if="followerList">{{followerList}}</div>
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Num Post </a>
-              </li>
-              <div v-if="n_post">{{n_post}}</div>
-            </ul>
-          </div>
-        </div>
         
-
-            
+          <div class="col col-sm-1" style="font-size: medium;">
+            Num Follower: 
+              <div></div>
+          </div>
+          <div class="col col-sm-1" style="font-size: medium; font-style: 
+          ;">
+            {{n_follower}}
+          </div>
+          <div class="col col-sm-1" style="font-size: medium;">
+            Num Followed:
+          </div>
+          <div class="col col-sm-1" style="font-size: medium;">
+            {{n_followed}}
+          </div>
+          <div class="col col-sm-1" style="font-size: medium;">
+            Num Post:
+          </div>
+          <div class="col col-sm-1" style="font-size: medium;">
+            {{n_posts}}
+          </div>
+        
+          
+        </div>      
 
       </div>   
 		    <RouterView />
