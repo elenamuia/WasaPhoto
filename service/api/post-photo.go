9e339b77ad4546153e91c29149aed5d7b7f8b821
Elena Muià <muia.1938610@studenti.uniroma1.it>
2023-05-24 12:10:05 +0200

    comment

--- service/api/post-photo.go
+++ service/api/post-photo.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"encoding/base64"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
@@ -33,24 +32,26 @@ func (rt *_router) uploadPhoto(w http.ResponseWriter, r *http.Request, ps httpro
 
 		return
 	}
+	/*
+		var base64Encoding string
 
-	var base64Encoding string
+		// Determine the content type of the image file
+		mimeType := http.DetectContentType(bytes)
 
-	// Determine the content type of the image file
-	mimeType := http.DetectContentType(bytes)
+		// Prepend the appropriate URI scheme header depending
+		// on the MIME type
+		switch mimeType {
+		case "image/jpeg":
+			base64Encoding += "data:image/jpeg;base64,"
+		case "image/png":
+			base64Encoding += "data:image/png;base64,"
+		}
+		resultEncoding := base64.StdEncoding.EncodeToString(bytes)
 
-	// Prepend the appropriate URI scheme header depending
-	// on the MIME type
-	switch mimeType {
-	case "image/jpeg":
-		base64Encoding += "data:image/jpeg;base64,"
-	case "image/png":
-		base64Encoding += "data:image/png;base64,"
-	}
-	resultEncoding := base64.StdEncoding.EncodeToString(bytes)
+		// Append the base64 encoded output
+		base64Encoding += resultEncoding
 
-	// Append the base64 encoded output
-	base64Encoding += resultEncoding
+	*/
 
 	// fileBytes is now a []byte containing the contents of the file
 
