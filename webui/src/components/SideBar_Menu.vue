da5253fff958902efa038332a6fe6b74be8ff706
Elena Muià <muia.1938610@studenti.uniroma1.it>
2023-09-07 21:26:10 +0200

    paging mainstream

--- webui/src/components/SideBar_Menu.vue
+++ webui/src/components/SideBar_Menu.vue
@@ -63,6 +63,18 @@ export default {
                 this.img = response.data;
                 this.closePhotoModal();
                 this.$router.push('/mainstream/' + this.$current_user.id);
+                request.onreadystatechange = function(){
+                    if (request.readyState === 4) {
+                        if(request.status === 200 && request.status.text ==='OK'){
+                            console.log("successful")
+                        }
+                        else{
+                            console.log("failed")
+                        }
+                    }
+                }
+			
+		
                 
             } catch (e) {
                 if (e.response && e.response.status === 404) {
